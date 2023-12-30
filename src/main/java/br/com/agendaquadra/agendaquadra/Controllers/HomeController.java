fa9bc0ac3f12510b2fc03798f8888e4b4198ebca
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2023-12-30 11:27:20 -0300

    Adicionando

--- src/main/java/br/com/agendaquadra/agendaquadra/Controllers/HomeController.java
+++ src/main/java/br/com/agendaquadra/agendaquadra/Controllers/HomeController.java
@@ -1,10 +1,20 @@
 package br.com.agendaquadra.agendaquadra.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.agendaquadra.agendaquadra.Models.Usuarios;
+import br.com.agendaquadra.agendaquadra.Repositorios.AdministradorRepo;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private AdministradorRepo repo;
 
     @GetMapping("/")
     public String home() {
@@ -14,5 +24,21 @@ public class HomeController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/cadcliente")
+    public String cadastrar(){
+        return "cadcliente";
+    }
+
+    /**
+     * @param model
+     * @return
+     */
+    @GetMapping("/admin")
+    public String admin(Model model){
+        List<Usuarios> usuarios = (List<Usuarios>)repo.findAll();
+        model.addAttribute("usuarios", usuarios);
+        return "listaAdmin";
     }
 }
