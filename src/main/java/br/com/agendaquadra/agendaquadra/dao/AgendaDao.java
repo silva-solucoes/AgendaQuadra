85ac29d806b81c272cff6785818169897ccf934c
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2024-01-02 21:40:43 -0300

    Finalização do Projeto

--- src/main/java/br/com/agendaquadra/agendaquadra/dao/AgendaDao.java
+++ src/main/java/br/com/agendaquadra/agendaquadra/dao/AgendaDao.java
@@ -1,9 +1,41 @@
 package br.com.agendaquadra.agendaquadra.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.agendaquadra.agendaquadra.Enums.Quadras;
 import br.com.agendaquadra.agendaquadra.Models.Agenda;
+import jakarta.transaction.Transactional;
 
 public interface AgendaDao extends JpaRepository<Agenda, Integer>{
     
+    List<Agenda> findByQuadra(Quadras quadra);
+
+    @Query("SELECT a FROM Agenda a WHERE a.quadra = :quadra ORDER BY CASE WHEN a.aceito = 'Pendente' THEN 0 WHEN a.aceito = 'Aceito' THEN 1 WHEN a.aceito = 'Negado' THEN 2 ELSE 3 END, a.id DESC")
+    List<Agenda> findByQuadraOrderByStatusAndIdDesc(Quadras quadra);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Agenda a SET a.aceito = 'Negado' WHERE a.id = :id AND a.aceito = 'Pendente'")
+    void negarAgendamento(@Param("id") Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Agenda a SET a.aceito = 'Aceito' WHERE a.id = :id AND a.aceito = 'Pendente'")
+    void aceitarAgendamento(@Param("id") Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Agenda a SET a.aceito = 'Negado' WHERE a.id = :id AND a.aceito = 'Aceito'")
+    void negarAgendamento2(@Param("id") Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Agenda a SET a.aceito = 'Aceito' WHERE a.id = :id AND a.aceito = 'Negado'")
+    void aceitarAgendamento2(@Param("id") Integer id);
+
 }
--- src/main/java/br/com/agendaquadra/agendaquadra/dao/UsuarioDao.java
+++ src/main/java/br/com/agendaquadra/agendaquadra/dao/UsuarioDao.java
@@ -1,9 +1,15 @@
 package br.com.agendaquadra.agendaquadra.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.agendaquadra.agendaquadra.Enums.TipoAcesso;
 import br.com.agendaquadra.agendaquadra.Models.Usuario;
+import jakarta.transaction.Transactional;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Integer> {
     
@@ -12,5 +18,19 @@ public interface UsuarioDao extends JpaRepository<Usuario, Integer> {
 
     @Query("SELECT u FROM Usuario u WHERE u.email = :email AND u.senha = :senha")
     public Usuario buscarLogin(String email, String senha);
+
+    List<Usuario> findByTipoacessoIn(List<TipoAcesso> tiposAcesso);
+
+    // Método para bloquear um usuário
+    @Transactional
+    @Modifying
+    @Query("UPDATE Usuario u SET u.ativo = 2 WHERE u.id = :id")
+    void bloquearUsuario(@Param("id") Integer id);
+
+    // Método para ativar um usuário
+    @Transactional
+    @Modifying
+    @Query("UPDATE Usuario u SET u.ativo = 1 WHERE u.id = :id")
+    void ativarUsuario(@Param("id") Integer id);
 
 }
